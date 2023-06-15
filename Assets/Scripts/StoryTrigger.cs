8d388fd0783bfcefc070205d9fe8afdbc250c9d1
st-Helicoprion <christiankwadranck30@gmail.com>
2023-06-16 00:24:15 +0800

    Preparing to merge with "added frog tongue animation"

--- Assets/Scripts/StoryTrigger.cs
+++ Assets/Scripts/StoryTrigger.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform targetPos, currPos;
+    public GameObject currMap, targetMap;
+    public Image blackScreen;
+   private void OnTriggerEnter(Collider other)
+   {
+        currPos.position = other.gameObject.transform.position;
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void MovePlayer()
+   {
+      Color panelColor = blackScreen.color;
+      panelColor.a-=0.25f*Time.deltaTime;
+      
+      currPos.position=targetPos.position;
+
+      if(panelColor.a<=0)
+      {
+        panelColor.a+=0.25f*Time.deltaTime;
+      }
+   }
+
+   public void ReplaceMap()
+   {
+     currMap.SetActive(false); targetMap.SetActive(true);
+   }
 }
