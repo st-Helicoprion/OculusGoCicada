4b6ae78ad0845a76e90553fe06068436fef53360
st-Helicoprion <christiankwadranck30@gmail.com>
2023-06-17 19:55:38 +0800

    Preparing to merge scale up

--- Assets/ReplaceWithPrefab.cs
+++ Assets/ReplaceWithPrefab.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
+
+#if UNITY_EDITOR
 public class ReplaceWithPrefab : EditorWindow
 {
     [SerializeField] private GameObject prefab;
@@ -55,3 +57,4 @@ public class ReplaceWithPrefab : EditorWindow
         EditorGUILayout.LabelField("Selection count: " + Selection.objects.Length);
     }
 }
+#endif
