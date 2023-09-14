fc37cbd65ae1f85dff1566f9eee0a05eb8afc2c2
st-Helicoprion <christiankwadranck30@gmail.com>
2023-09-15 02:07:42 +0800

    Switched to "Enlarge sonar circle over time" and added pulsing sonar

---
+++ Assets/Scripts/RepeatingSonar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class RepeatingSonar : MonoBehaviour
+{
+    public Transform playerPos;
+    public GameObject limitSonar, prefabSonar;
+    public float count, releaseTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+        if(count > releaseTime) { ReleaseSonarWave(); }
+
+        limitSonar.transform.position = playerPos.position;
+    }
+
+    public void ReleaseSonarWave()
+    {
+        count = 0;
+
+        if (limitSonar.transform.localScale.x > 5)
+        {
+            Instantiate(prefabSonar, limitSonar.transform.position, Quaternion.identity);
+        }
+        else return;
+    }
+
+    
+}
