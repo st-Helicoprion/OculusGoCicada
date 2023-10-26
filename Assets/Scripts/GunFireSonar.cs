cd6f334c8325ba2768d5c490051f3e648738ad33
HaoXuan Hong <96000131+HurryPeter@users.noreply.github.com>
2023-10-27 00:00:56 +0800

    竹槍機制 大致完成1026

--- Assets/Scripts/GunFireSonar.cs
+++ Assets/Scripts/GunFireSonar.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class GunFireSonar : MonoBehaviour
 {
     [SerializeField] GameObject BulletPrefab;
-    [SerializeField] Transform BulletTrashCan;
     [SerializeField] Transform BulletSpawnPoint;
-    [SerializeField] float ExistTime=5;
+    [SerializeField] float ExistTime = 1;
+    [SerializeField] float SpawnSonar = 10;
+    [SerializeField] float TimeInterval = 0.5f;
     GameObject bulletTemp;
+    InputReader inputReader;
     void Start()
     {
-        var inputReader = Resources.Load<InputReader>("Input Reader Prefab");
+        inputReader = Resources.Load<InputReader>("Input Reader Prefab");
         inputReader.GunFireSonar += SpawnBullet;
     }
     void SpawnBullet()
     {
-        bulletTemp=GameObject.Instantiate(BulletPrefab,BulletSpawnPoint);
-        bulletTemp.transform.SetParent(BulletTrashCan);//Leave Bullets at current palce
-        Debug.Log("spawn");
-        Destroy(bulletTemp,ExistTime);
-        // GameObject.Instantiate()
+        Vector3 CurrentSpawnLocation;
+        Quaternion CurrentSpawnRotation;
+        CurrentSpawnLocation = BulletSpawnPoint.position;//存取現在的位置和旋轉角度
+        CurrentSpawnRotation= BulletSpawnPoint.rotation;
+        StartCoroutine(SpawnBulletRepeat(CurrentSpawnLocation,CurrentSpawnRotation));
+    }
+
+    IEnumerator SpawnBulletRepeat(Vector3 vector3,Quaternion quaternion)
+    {
+        //SpawnBullet觸發後，每經過TimeInterval的時間，生成一個聲波，總共生SpawnSonar個
+        for (int i = 0; i < SpawnSonar; i++)
+        {
+            bulletTemp = GameObject.Instantiate(BulletPrefab, vector3,quaternion);
+            Debug.Log("spawn");
+            Destroy(bulletTemp, ExistTime);
+            yield return new WaitForSecondsRealtime(TimeInterval);
+        }
     }
 
 }
